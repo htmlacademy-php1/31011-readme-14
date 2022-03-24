bdbb8d16fd68e4db9639888bbd068fdc11298712
Blacking84 <alexey@lugachev.ru>
2022-03-25 00:00:48 +0700

    Доработка функции конвертации времени

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ $is_auth = rand(0, 1);
 $user_name = 'Алексей'; // укажите здесь ваше имя
 
 $posts = [
-    ['id' => 0, 'header' => 'Цитата', 'type' => 'post-quote', 'post' => 'Мы в жизни любим только раз, а после ищем лишь похожих', 'name_user' => 'Лариса', 'avatar' => 'userpic-larisa-small.jpg'],
-    ['id' => 1, 'header' => 'Игра престолов', 'type' => 'post-text', 'post' => 'Не могу дождаться начала финального сезона своего любимого сериала! В массиве с постами для текстового поста укажите в содержимом очень длинный текст. Проверьте, что этот текст корректно обрезается с добавлением ссылки. Затем проверьте короткий текст, который должен отображаться без изменений. В массиве с постами для текстового поста укажите в содержимом очень длинный текст. Проверьте, что этот текст корректно обрезается с добавлением ссылки. Затем проверьте короткий текст, который должен отображаться без изменений.', 'name_user' => 'Владик', 'avatar' => 'userpic.jpg'],
-    ['id' => 2, 'header' => 'Наконец, обработал фотки!', 'type' => 'post-photo', 'post' => 'rock-medium.jpg', 'name_user' => 'Виктор', 'avatar' => 'userpic-mark.jpg'],
-    ['id' => 3, 'header' => 'Моя мечта', 'type' => 'post-photo', 'post' => 'coast-medium.jpg', 'name_user' => 'Лариса', 'avatar' => 'userpic-larisa-small.jpg'],
-    ['id' => 4, 'header' => 'Лучшие курсы', 'type' => 'post-link', 'post' => 'www.htmlacademy.ru', 'name_user' => 'Владик', 'avatar' => 'userpic.jpg']
+    ['header' => 'Цитата', 'type' => 'post-quote', 'post' => 'Мы в жизни любим только раз, а после ищем лишь похожих', 'name_user' => 'Лариса', 'avatar' => 'userpic-larisa-small.jpg'],
+    ['header' => 'Игра престолов', 'type' => 'post-text', 'post' => 'Не могу дождаться начала финального сезона своего любимого сериала! В массиве с постами для текстового поста укажите в содержимом очень длинный текст. Проверьте, что этот текст корректно обрезается с добавлением ссылки. Затем проверьте короткий текст, который должен отображаться без изменений. В массиве с постами для текстового поста укажите в содержимом очень длинный текст. Проверьте, что этот текст корректно обрезается с добавлением ссылки. Затем проверьте короткий текст, который должен отображаться без изменений.', 'name_user' => 'Владик', 'avatar' => 'userpic.jpg'],
+    ['header' => 'Наконец, обработал фотки!', 'type' => 'post-photo', 'post' => 'rock-medium.jpg', 'name_user' => 'Виктор', 'avatar' => 'userpic-mark.jpg'],
+    ['header' => 'Моя мечта', 'type' => 'post-photo', 'post' => 'coast-medium.jpg', 'name_user' => 'Лариса', 'avatar' => 'userpic-larisa-small.jpg'],
+    ['header' => 'Лучшие курсы', 'type' => 'post-link', 'post' => 'www.htmlacademy.ru', 'name_user' => 'Владик', 'avatar' => 'userpic.jpg']
 ];
 
 // Заполнение врЕменного массива данных временнЫми метками
 foreach ($posts as $key => $post) {
-    $posts[$key]['post_date'] = generate_random_date($post['id']);
+    $posts[$key]['post_date'] = generate_random_date($key);
 }
 
 // Функция для обрезки пользовательских постов с добавлением ссылки на полный текст поста
@@ -42,24 +42,31 @@ function cropping_post ($post, $lenght=300) {
 
 // Функция для перевода времени поста в относительный формат
 function convert_date_relative_format($date) {
-    $date = time() - strtotime($date);
-    if ($date/60 < 60) {
-        $date = floor($date/60);
-        $date .= " " . get_noun_plural_form($date, "минута", "минуты", "минут") . " назад";
-    } elseif ($date/60 >= 60 && $date/60/60 < 24) {
-        $date = floor($date/60/60);
-        $date .= " " . get_noun_plural_form($date, "час", "часа", "часов") . " назад";
-    } elseif ($date/60/60 >= 24 && $date/60/60/24 < 7) {
-        $date = floor($date/60/60/24);
-        $date .= " " . get_noun_plural_form($date, "день", "дня", "дней") . " назад";
-    } elseif ($date/60/60/24 >= 7 && $date/60/60/24/7 < 5) {
-        $date = floor($date/60/60/24/7);
-        $date .= " " . get_noun_plural_form($date, "неделя", "недели", "недель") . " назад";
-    } elseif ($date/60/60/24/7 >= 5) {
-        $date = floor($date/60/60/24/30);
-        $date .= " " . get_noun_plural_form($date, "месяц", "месяца", "месяцев") . " назад";
+    $sec = time() - strtotime($date);
+    $min = $sec / 60;
+    $hour = $min / 60;
+    $day = $hour / 24;
+    $week = $day / 7;
+    $month = $week / 30;
+
+    if ($min < 60) {
+        $date = floor($min);
+        $date .= " " . get_noun_plural_form($date, "минута", "минуты", "минут");
+    } elseif ($min >= 60 && $hour < 24) {
+        $date = floor($hour);
+        $date .= " " . get_noun_plural_form($date, "час", "часа", "часов");
+    } elseif ($hour >= 24 && $day < 7) {
+        $date = floor($day);
+        $date .= " " . get_noun_plural_form($date, "день", "дня", "дней");
+    } elseif ($day >= 7 && $week < 5) {
+        $date = floor($week);
+        $date .= " " . get_noun_plural_form($date, "неделя", "недели", "недель");
+    } elseif ($week >= 5) {
+        $date = floor($month);
+        $date .= " " . get_noun_plural_form($date, "месяц", "месяца", "месяцев");
     }
-    return $date;
+    
+    return $date . " назад";
 }
 
 
