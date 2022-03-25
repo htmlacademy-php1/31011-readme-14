624022f93384fb55cf872da29194f0634a743ec3
Blacking84 <alexey@lugachev.ru>
2022-03-25 10:01:59 +0700

    Обновление схемы БД

--- schema.sql
+++ schema.sql
@@ -1,68 +1,68 @@
-CREATE DATABASE readme
+CREATE DATABASE `readme`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 
-USE readme;
+USE `readme`;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    login CHAR(255) NOT NULL UNIQUE,
-    email CHAR(128) NOT NULL UNIQUE,
-    password CHAR(64),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    avatar CHAR(64)
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` char(255) NOT NULL,
+  `email` char(128) NOT NULL,
+  `password` char(64) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `avatar` char(64)
 );
 
-CREATE TABLE posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    type_id INT,
-    header CHAR(255),
-    post TEXT(65535),
-    author_quote CHAR(255),
-    images_link CHAR(64),
-    video_link CHAR(255),
-    site_link CHAR(255),
-    hashtag TEXT(1000),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    view INT
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `header` char(255) NOT NULL,
+  `post` mediumtext NOT NULL,
+  `author_quote` char(255),
+  `images_link` char(64),
+  `video_link` char(255),
+  `site_link` char(255),
+  `hashtag` text,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `view` int(11) NOT NULL DEFAULT 0
 );
 
-CREATE TABLE comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    post_id INT,
-    post TEXT(65535),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `post` mediumtext NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 );
 
-CREATE TABLE likes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    post_id INT
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
 );
 
-CREATE TABLE subscribes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    subscribed_id INT
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `addressee_id` int(11) NOT NULL,
+  `message` mediumtext NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 );
 
-CREATE TABLE messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    addressee_id INT,
-    message TEXT(65535),
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `subscribes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `subscribed_id` int(11) NOT NULL
 );
 
-CREATE TABLE hashtags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    hashtag CHAR(255)
+CREATE TABLE `type_content` (
+  `id` int(11) NOT NULL,
+  `name` char(255) NOT NULL,
+  `icon` char(64) NOT NULL
 );
 
-CREATE TABLE type_content (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(255),
-    icon CHAR(64)
+CREATE TABLE `hashtags` (
+  `id` int(11) NOT NULL,
+  `hashtag` char(255) NOT NULL
 );
