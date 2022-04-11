c119eecc21aa9952f6a0829fe6d4d529aae0fcec
Blacking84 <alexey@lugachev.ru>
2022-04-11 17:40:47 +0700

    Доработки

--- db_helpers.php
+++ db_helpers.php
@@ -134,4 +134,53 @@ function get_posts($link, $where, $order, $limit = '') {
         return $posts;
     }
 }
+
+/**
+ * Получение постов из таблицы БД с выборкой, сортировкой и пагинацией
+ * и с подписками текущего пользователя
+ * @param $link ресурс подключения к БД
+ * @param string $where часть SQL запроса WHERE
+ * @param string $order часть SQL запроса ORDER
+ * @param string $limit часть SQL запроса LIMIT
+ * @param integer $user_id id пользователя для которого делаем выборку
+ * @return array
+ */
+function get_posts_by_subscribed($link, $where, $order, $user_id, $limit = '') {
+
+    if (!$where) {
+        $where = "WHERE s1.user_id = " . $user_id;
+    } else {
+        $where = $where . " AND s1.user_id = " . $user_id;
+    }
+    
+    $sql = <<<SQL
+        SELECT p.id, u.id user_id, u.login, u.email, u.avatar, c.type, p.header, p.post, p.date, u.date reg_date,
+            p.author_quote, p.image_link, p.video_link, p.site_link, p.view,
+            COUNT(DISTINCT com.id) comments_count, COUNT(DISTINCT l.user_id) likes_count,
+            COUNT(DISTINCT s.user_id) subscribed, COUNT(DISTINCT p1.id) posts, COUNT(DISTINCT s1.user_id) me_subscribed, s1.user_id suserid
+        FROM `posts` p
+        INNER JOIN `users` u ON p.user_id = u.id
+        INNER JOIN `content_types` c ON p.type_id = c.id
+        LEFT JOIN `comments` com ON p.id = com.post_id
+        LEFT JOIN `likes` l ON p.id = l.post_id
+        LEFT JOIN `subscriptions` s ON u.id = s.subscribed_id
+        LEFT JOIN `posts` p1 ON p.user_id = p1.user_id
+        LEFT JOIN `posts_hashtags` ph ON p.id = ph.post_id
+        LEFT JOIN `hashtags` h ON ph.hashtag_id = h.id
+        LEFT JOIN `subscriptions` s1 ON s1.user_id = $user_id AND s1.subscribed_id = u.id
+        $where
+        GROUP BY p.id
+        $order
+        $limit;
+    SQL;
+
+    $posts = db_get_all($link, $sql);
+    if (!$posts) {
+        return [];
+    } elseif (count($posts) === 1) {
+        return $posts[0];
+    } else {
+        return $posts;
+    }
+}
 ?>
--- feed.php
+++ feed.php
@@ -10,13 +10,13 @@ $content_types = get_content_types($link);
 
 $add_sql = "";
 $ctype = filter_input(INPUT_GET, 'ctype');
+$where_sql = '';
 if ($ctype) {
-    $add_sql = " AND p.type_id = " . $ctype;
+    $where_sql = "WHERE p.type_id = " . $ctype;
 }
 
-$where_sql = "WHERE p.user_id IN (SELECT s.subscribed_id FROM `subscriptions` s WHERE s.user_id = " . $_SESSION['user_id'] . ") " . $add_sql;
 $order_sql = 'ORDER BY p.date DESC';
-$posts = get_posts($link, $where_sql, $order_sql);
+$posts = get_posts_by_subscribed($link, $where_sql, $order_sql, $_SESSION['user_id']);
 
 $page_content = include_template('feed.php', ['content_types' => $content_types, 'ctype' => $ctype, 'posts' => $posts]);
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'readme: моя лента']);
